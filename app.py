b5563c110f3b623180a2c8c094b355efd9c13ebf
Richard Molnar <molnarriso@gmail.com>
2023-03-12 18:20:17 +0100

    Added user input of prompt for generation

--- app.py
+++ app.py
@@ -10,11 +10,16 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @app.route("/stalagmite", methods=["GET"])
 def index():
+    p = request.args.get("prompt")
     res=os.getenv("RESOLUTION")
-    response = openai.Image.create(
-    prompt=os.getenv("PROMPT"),
-    n=1,
-    size=f"{res}x{res}"
-	)
-    image_url = response['data'][0]['url']
-    return render_template("stalagmite.html", image_url=image_url)
+    if p is None :
+        p=os.getenv("PROMPT")
+        return render_template("stalagmite.html",res=res,prompt=p)
+    else :
+        response = openai.Image.create(
+        prompt=p,
+        n=1,
+        size=f"{res}x{res}"
+        )
+        image_url = response['data'][0]['url']
+        return render_template("stalagmite.html", image_url=image_url,res=res,prompt=p)
