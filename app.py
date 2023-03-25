4cd590bbfd04aefbf9ebaf1fe0c5640fc7a82d16
Richard Molnar <molnarriso@gmail.com>
2023-03-25 18:20:43 +0100

    Added async loading

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import openai
 from flask import Flask, redirect, render_template, request, url_for
 import logging
 from logging.config import dictConfig
+import time, threading
 
 app = Flask(__name__)
 
@@ -31,6 +32,30 @@ dictConfig({
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+cache = {
+    "nextStatus" : "empty",
+    "next" : "",
+}
+def generateImage(p,res):
+    response = openai.Image.create(
+                prompt=p,
+                n=1,
+                size=f"{res}x{res}"
+            )
+    image_url = response['data'][0]['url']
+    return image_url
+
+def loadNextThread(p,res):
+    app.logger.info("Loading asynchrounsly "+p+" ...")
+    image_url = generateImage(p,res)
+    cache['next'] = image_url
+    cache['nextStatus'] = "ready"
+    app.logger.info("Next image ready")
+    return
+
+def loadNext(p,res) :
+    threading.Thread(target=loadNextThread,args=(p,res)).start()
+
 @app.route("/stalagmite", methods=["GET"])
 def index():
     p = request.args.get("prompt")
@@ -39,11 +64,24 @@ def index():
         p=os.getenv("PROMPT")
         return render_template("stalagmite.html",res=res,prompt=p)
     else :
-        response = openai.Image.create(
-        prompt=p,
-        n=1,
-        size=f"{res}x{res}"
-        )
-        image_url = response['data'][0]['url']
+        image_url = ""
+        if cache['nextStatus'] == "empty" :
+            cache['nextStatus'] = "loading"
+            loadNext(p,res)
+        else :
+            if cache['nextStatus'] == "loading" :
+                waited = 0
+                while cache['nextStatus'] == "loading" and waited < 30:
+                    app.logger.info("Next image is still loading, sleeping...")
+                    waited += 1
+                    time.sleep(1)
+            if cache['nextStatus'] == "ready" :
+                image_url = cache['next']
+                cache['nextStatus'] = "loading"
+                loadNext(p,res)
+
+        if image_url == "" :
+            image_url = generateImage(p,res)
+
         app.logger.info(image_url)
         return render_template("stalagmite.html", image_url=image_url,res=res,prompt=p)
